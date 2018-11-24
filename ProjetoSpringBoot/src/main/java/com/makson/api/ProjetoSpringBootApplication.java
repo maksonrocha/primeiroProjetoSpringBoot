c1c8dab7726128e0c48aa53eeba2439894e76292
Makson Rocha <maksonrochati@gmail.com>
2018-11-24 20:03:58 -0300

    Arquivos Iniciais do Projeto

---
+++ ProjetoSpringBoot/src/main/java/com/makson/api/ProjetoSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.makson.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootApplication.class, args);
+	}
+}
